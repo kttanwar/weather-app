8d0b4f8f2043ee31a9b258c6016164fea1f6c0ed
kttanwar <kttanwar35@gmail.com>
2021-06-06 16:34:57 +0530

    Geocoding

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ request({url: url, json: true}, (error, response) => {
     // console.log(response.body.current)
     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.' )
 })
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia29uYXJrIiwiYSI6ImNrcGtzemtmbDF5bDMyd3JpajJqMnVlbzIifQ.SaRTjKAtR619JJLHLgZb4g&limit=1'
+
+request({url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
+})
