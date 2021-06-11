e0831ff859205b592151cf7f6f63909ecf1012ea
kttanwar <kttanwar35@gmail.com>
2021-06-11 19:45:42 +0530

    Destructuring and Shorthand

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=04d37208e81a7d28b344e8ad5ea21ed9&query=' + latitude + ',' + longitude +'&units=f'
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if(error){
             callback('Unable to connect to weather service!', undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to find location!', undefined)
         }else{
-            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
         }
     })
 }
